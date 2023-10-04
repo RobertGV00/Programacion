e2094d6e49ec6752878f557601db7a0014e6cdf1
RobertGV00 <robertgvclase@gmail.com>
2023-10-04 19:02:54 +0200

    Actualizado

---
+++ src/PrimeraEvaluacion/Tema02/ejercicios/ejerciciosSobreOperadores/Ejercicio29.java
@@ -0,0 +1,12 @@
+package PrimeraEvaluacion.Tema02.ejercicios.ejerciciosSobreOperadores;
+
+import java.util.Scanner;
+
+public class Ejercicio29 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int num1 = teclado.nextInt();
+        int num2 = teclado.nextInt();
+        int num3 = teclado.nextInt();
+    }
+}
