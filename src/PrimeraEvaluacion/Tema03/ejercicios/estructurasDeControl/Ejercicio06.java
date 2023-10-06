6d89ec8225cb4e82f047aa998d1769d2f1f147d8
RobertGV00 <robertgvclase@gmail.com>
2023-10-06 20:28:26 +0200

    Actualizado

---
+++ src/PrimeraEvaluacion/Tema03/ejercicios/estructurasDeControl/Ejercicio06.java
@@ -0,0 +1,21 @@
+package PrimeraEvaluacion.Tema03.ejercicios.estructurasDeControl;
+
+import java.util.Scanner;
+
+public class Ejercicio06 {
+    /*
+    Escribe un programa que pida tres números y los muestre ordenados (de menor a mayor).
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca 3 numeros");
+        int num1 = teclado.nextInt();
+        int num2 = teclado.nextInt();
+        int num3 = teclado.nextInt();
+        int mayor = num1;
+        int menor = num2;
+        if (num1 < num2);{
+            
+        }
+    }
+}
---
+++ src/PrimeraEvaluacion/Tema03/ejercicios/estructurasDeControl/Ejercicio13If.java
@@ -0,0 +1,29 @@
+package PrimeraEvaluacion.Tema03.ejercicios.estructurasDeControl;
+
+import java.util.Scanner;
+
+public class Ejercicio13If {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Indique un numero de direccion: ");
+        System.out.println("0. No mover");
+        System.out.println("1. Arriba");
+        System.out.println("2. Abajo");
+        System.out.println("3. Izquierda");
+        System.out.println("4. Derecha");
+    int numero = teclado.nextInt();
+        if (numero == 0){
+            System.out.println("No mover");
+        } else if (numero == 1 ) {
+            System.out.println("Arriba");
+        } else if (numero == 2) {
+            System.out.println("Abajo");
+        } else if (numero == 3) {
+            System.out.println("Izquierda");
+        } else if (numero == 4) {
+            System.out.println("Derecha");
+        } else {
+            System.out.println("¡error!");
+        }
+    }
+}
