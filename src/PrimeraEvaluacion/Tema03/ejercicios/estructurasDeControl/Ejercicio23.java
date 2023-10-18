1570800f99420ef55582b7909972e6e397374217
RobertGV00 <robertgvclase@gmail.com>
2023-10-18 19:28:06 +0200

    Actualizado

---
+++ src/PrimeraEvaluacion/Tema03/ejercicios/estructurasDeControl/Ejercicio23.java
@@ -0,0 +1,18 @@
+package PrimeraEvaluacion.Tema03.ejercicios.estructurasDeControl;
+
+import java.util.Scanner;
+
+public class Ejercicio23 {
+    //[FOR ANIDADO] Escribe un programa que, dado un número n introducido por teclado, dibuje un cuadrado de dimensiones n * n, con un carácter también introducido por teclado. Ejemplo:
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int n = teclado.nextInt();
+char caracter = teclado.next().charAt(0);
+        for (int i = 1; i <= n ; i++) {
+            System.out.println();
+            for (int j = 1; j <= n ; j++) {
+                System.out.print(caracter + " ");
+            }
+        }
+    }
+}
---
+++ src/PrimeraEvaluacion/Tema03/ejercicios/estructurasDeControl/Ejercicio26.java
@@ -0,0 +1,20 @@
+package PrimeraEvaluacion.Tema03.ejercicios.estructurasDeControl;
+
+import java.util.Scanner;
+
+public class Ejercicio26 {
+    //Hacer un programa que calcule la suma y el producto de los 30 primeros números naturales. (El 0 no cuenta)
+    public static void main(String[] args) {
+        int n = 1;
+        int suma = 0;
+        for (int i = n; i < 31; i++) {
+            suma = suma + i;
+            System.out.println(   i + " = " + suma);
+        }
+        int producto = 1;
+        for (int i = 1; i < 31; i++) {
+            producto = producto * i;
+            System.out.println(producto);
+        }
+    }
+}
