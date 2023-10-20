00d11913953480cbd953c8088b0177634ad17134
RobertGV00 <robertgvclase@gmail.com>
2023-10-20 21:01:42 +0200

    Actualizado

---
+++ src/PrimeraEvaluacion/Tema03/ejercicios/estructurasDeControl/Ejercicio35.java
@@ -0,0 +1,7 @@
+package PrimeraEvaluacion.Tema03.ejercicios.estructurasDeControl;
+
+public class Ejercicio35 {
+    //Dada una secuencia de números enteros (que finaliza con el número 0), encuentra el elemento más grande de la secuencia. El número 0 no está incluido en la secuencia, sólo se utiliza para finalizar el programa.
+
+
+}
